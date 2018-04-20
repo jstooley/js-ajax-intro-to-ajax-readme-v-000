38d0f946a9c5781365aeb449e7a8bc88adfa5193
Justin Tooley <jstooley823@gmail.com>
2018-04-20 16:37:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ function showRepositories(event, data) {
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
+function showCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
+}
 function getCommits(el) {
   const name = el.dataset.repo
   const req = new XMLHttpRequest()
@@ -17,8 +22,4 @@ function getCommits(el) {
   req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
   req.send()
 }
-function showCommits() {
-  const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
-  document.getElementById("commits").innerHTML = commitsList
-}
+
