d2a96fa1f6f10f617d7c57bd148ba9bf907cd061
Justin Tooley <jstooley823@gmail.com>
2018-04-20 16:12:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function getRepositories() {
   const req = new XMLHttpRequest()
   req.open("GET", 'https://api.github.com/users/octocat/repos')
   req.send()
-  console.log(req.html)
+  console.log(req.innerHTML)
 }
